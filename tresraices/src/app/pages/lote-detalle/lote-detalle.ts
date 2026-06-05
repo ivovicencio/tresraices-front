38d26fb4bcb434c2484341b58861fb6efa02af1b
ivovicencio <frikimaniaco28@gmail.com>
2026-06-05 15:42:22 -0300

    base del proyecto

---
+++ tresraices/src/app/pages/lote-detalle/lote-detalle.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lote-detalle',
+  imports: [],
+  templateUrl: './lote-detalle.html',
+  styleUrl: './lote-detalle.css',
+})
+export class LoteDetalle {}
---
+++ tresraices/src/app/pages/mapa/mapa.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa',
+  imports: [],
+  templateUrl: './mapa.html',
+  styleUrl: './mapa.css',
+})
+export class Mapa {}
